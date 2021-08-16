af67aee3f8028df52db87396b305bd7a39d58b08
Stone <stone.dummett@gmail.com>
2021-08-16 20:59:46 +0200

    Created struct for stacks and functions that creates the 2 stacks

--- push_swap.c
+++ push_swap.c
@@ -6,16 +6,29 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 20:39:16 by sdummett          #+#    #+#             */
-/*   Updated: 2021/08/16 20:44:07 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/08/16 20:58:11 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+t_stack	*create_stacks(int ac)
+{
+	t_stack	*stacks;
+
+	stacks = malloc(sizeof(t_stack));
+	stacks->nb_elem = ac - 1;
+	stacks->a = malloc(sizeof(int) * stacks->nb_elem);
+	stacks->b = malloc(sizeof(int) * stacks->nb_elem);
+	return (stacks);
+}
+
 int main(int ac, char **av)
 {
-	(void)ac;
+	t_stack	*stacks;
 	(void)av;
+
+	stacks = create_stacks(ac);
 	printf("Hello world!\n");
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 20:16:12 by sdummett          #+#    #+#             */
-/*   Updated: 2021/08/16 20:39:04 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/08/16 20:54:05 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 # include <stdio.h> // <- to delete
 # include <unistd.h>
 # include <stdlib.h>
+
+typedef struct s_stack
+{
+	int	*a;
+	int	*b;
+	int	nb_elem;
+}	t_stack;
 
 #endif
 
