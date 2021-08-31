36128294f38c7ed052148b023e61404478db4d60
Stone <stone.dummett@gmail.com>
2021-08-31 10:30:43 +0200

    The stick_them function gets the index of the extreme values

--- algorithm/stick_extreme_numbers.c
+++ algorithm/stick_extreme_numbers.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 09:53:54 by sdummett          #+#    #+#             */
-/*   Updated: 2021/08/31 10:14:46 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/08/31 10:29:27 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	is_stick(t_stacks * stacks)
 {
-		unsigned int	i;
+	unsigned int	i;
 	int				min;
 	int				max;
 
@@ -60,11 +60,52 @@ int	is_stick(t_stacks * stacks)
 }
 
 
+unsigned int	get_index_min(t_stacks *stacks)
+{
+	unsigned int	i;
+	int				min;
+
+
+	min = get_min_number(stacks);
+	i = 0;
+	while (i < stacks->nb_elem_a)
+	{
+		if (stacks->a[i] == min)
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	get_index_max(t_stacks *stacks)
+{
+	unsigned int	i;
+	int				max;
+
+	max = get_max_number(stacks);
+	i = 0;
+	while (i < stacks->nb_elem_a)
+	{
+		if (stacks->a[i] == max)
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
+void	stick_them(t_stacks *stacks)
+{
+	unsigned int	i_min;
+	unsigned int	i_max;
+
+	i_min = get_index_min(stacks);
+	i_max = get_index_max(stacks);
+	printf("i_min: %d |i_max: %d\n", i_min, i_max);
+}
+
 void	stick_extreme_numbers(t_stacks *stacks)
 {
-
 	printf("nb_elem_a: %d\n", stacks->nb_elem_a);
 	if (is_stick(stacks) == 0)
 		stick_them(stacks);
-
 }
