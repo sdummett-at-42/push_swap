5f3a7ec65212fe95c2230e79be7e9b965104a727
Stone <stone.dummett@gmail.com>
2021-09-01 21:36:34 +0200

    The stack A sort is almost done, the algo manage to sort a value with next greater value

--- algorithm/sort_curr_biggest_number.c
+++ algorithm/sort_curr_biggest_number.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 14:54:48 by sdummett          #+#    #+#             */
-/*   Updated: 2021/09/01 19:33:26 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/09/01 21:32:31 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,16 +41,65 @@ int	is_well_placed(t_stacks *stacks, int curr_big_nb, int next_big_nb)
 	return (1);
 }
 
-void	move_ra(t_stacks *stacks, int curr_big_nb)
-{
-	while (stacks->a[stacks->nb_elem_a - 2] != curr_big_nb)
-		rotate_a(stacks);
-}
-
-void	move_rra(t_stacks *stacks, int curr_big_nb)
+void	move_ra(t_stacks *stacks, int curr_big_nb, t_move_2 *move)
 {
 	while (stacks->a[stacks->nb_elem_a - 1] != curr_big_nb)
+		rotate_a(stacks);
+	move->ra = 0;
+	move->rra = 0;
+}
+
+void	move_rra(t_stacks *stacks, int curr_big_nb, t_move_2 *move)
+{ 
+	while (stacks->a[stacks->nb_elem_a - 1] != curr_big_nb)
 		reverse_rotate_a(stacks);
+	move->ra = 0;
+	move->rra = 0;
+}
+
+void	how_to_stick_values(t_stacks * stacks, t_move_2 *move, int curr_big_nb, int next_big_nb)
+{
+	unsigned int	i_curr_big_nb;
+	unsigned int	i_next_big_nb;
+
+	i_curr_big_nb = get_number_index(stacks, curr_big_nb);
+	i_next_big_nb = get_number_index(stacks, next_big_nb);
+	if (i_curr_big_nb - i_next_big_nb - 1 > i_next_big_nb)
+		move->rra = 1;
+	else
+		move->ra = 1;
+}
+
+void	stick_values(t_stacks *stacks, t_move_2 *move, int curr_big_nb, int next_big_nb)
+{
+	unsigned int	i_curr_big_nb;
+	unsigned int	i_next_big_nb;
+
+	i_curr_big_nb = get_number_index(stacks, curr_big_nb);
+	i_next_big_nb = get_number_index(stacks, next_big_nb);
+	if (move->ra == 1)
+	{
+		while (1)
+		{
+			swap_a(stacks);
+			if (stacks->a[stacks->nb_elem_a - 3] == next_big_nb)
+				break ;
+			rotate_a(stacks);
+		}
+	}
+	else if (move->rra == 1)
+	{
+		while (1)
+		{
+			reverse_rotate_a(stacks);
+			swap_a(stacks);
+			if (stacks->a[stacks->nb_elem_a - 2] == next_big_nb)
+				break ;
+		}
+	}
+	move->ra = 0;
+	move->rra = 0;
+	print_stacks(stacks);
 }
 
 void	sort_curr_biggest_number(t_stacks *stacks, int curr_big_nb)
@@ -68,13 +117,14 @@ void	sort_curr_biggest_number(t_stacks *stacks, int curr_big_nb)
 	{
 		if (is_well_placed(stacks, curr_big_nb, next_big_nb) == 0)
 		{
-			printf("curr_big: %d | next_big: %d\n", curr_big_nb, next_big_nb);
 			print_stacks(stacks);
 			if (move.ra)
-				move_ra(stacks, curr_big_nb);
+				move_ra(stacks, curr_big_nb, &move);
 			else if (move.rra == 1)
-				move_rra(stacks, curr_big_nb);
+				move_rra(stacks, curr_big_nb, &move);
 			print_stacks(stacks);
+			how_to_stick_values(stacks, &move, curr_big_nb, next_big_nb);
+			stick_values(stacks, &move, curr_big_nb, next_big_nb);
 			break ;
 		}
 	}
--- algorithm/sort_stack_a.c
+++ algorithm/sort_stack_a.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 12:11:58 by sdummett          #+#    #+#             */
-/*   Updated: 2021/09/01 14:54:23 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/09/01 19:38:02 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	sort_stack_a(t_stacks *stacks)
 	int	curr_biggest_nb = find_biggest_number(stacks, get_max_number(stacks));
 	while (is_sorted(stacks) != 1)
 	{
+		// Not sure about this first condition
 		if (stacks->a[stacks->nb_elem_a - 1] > stacks->a[stacks->a[stacks->nb_elem_a - 2]])
 			swap_a(stacks);
 		else
