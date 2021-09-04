297a5fb36219a6c8657abd195092861ec4637845
Stone <stone.dummett@gmail.com>
2021-09-04 16:07:43 +0200

    Fix leaks

--- utils/clean_exit.c
+++ utils/clean_exit.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 22:22:01 by sdummett          #+#    #+#             */
-/*   Updated: 2021/08/30 19:21:32 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/09/04 16:06:40 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 
 void	clean_exit(t_stacks *stacks)
 {
+	unsigned int	i;
+
+	i = 0;
+	while (i < stacks->nb_elem_total)
+	{
+		free(stacks->best_moves[i]);
+		i++;
+	}
+	free(stacks->best_moves);
+	free(stacks->moves_a);
+	free(stacks->moves_b);
 	free(stacks->a);
 	free(stacks->b);
 	free(stacks->sorted->list);
