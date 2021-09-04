5589148faf609f77833d6726a1179bda980f099a
Stone <stone.dummett@gmail.com>
2021-09-04 15:02:44 +0200

    Corrected condition when the stack A is circularly sorted in order to do the best instructions to move MIN at the top of the stack

--- algorithm/how_to_move_pseudo_sorted.c
+++ algorithm/how_to_move_pseudo_sorted.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 11:28:03 by sdummett          #+#    #+#             */
-/*   Updated: 2021/09/01 11:42:47 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/09/04 15:00:06 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	how_to_move_pseudo_sorted(t_stacks *stacks, t_move_2 *move_2)
 	unsigned int	i_min;
 
 	i_min = get_index_min(stacks);
-	if (i_min < (stacks->nb_elem_a - 1) - i_min)
+	if (i_min < stacks->nb_elem_a / 2)
 		move_2->rra = 1;
 	else
 		move_2->ra = 1;
